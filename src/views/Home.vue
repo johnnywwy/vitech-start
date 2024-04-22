53b1f35d823fe77acb7217e7249b6721bb0661a3
wuwenyu <johnnywwy@gmail.com>
2024-04-22 10:58:49 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
+
+<script setup lang='ts'>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<style scoped></style>
