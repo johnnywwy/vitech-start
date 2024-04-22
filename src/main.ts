4b2cf769976ac3c696b95540097813d18406b661
wuwenyu <johnnywwy@gmail.com>
2024-04-22 11:04:01 +0800

    feat：引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
-// import "./style.css";
+import { createPinia } from "pinia";
+
+import "reset-css";
+
 import App from "./App.vue";
 import router from "./router";
 
+const pinia = createPinia();
+
 const app = createApp(App);
 app.use(router); //注册路由
+app.use(pinia); // 注册pinia
 app.mount("#app");
